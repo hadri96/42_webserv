6f59987cf57109688d4b5e4d6b60ca72e902fda1
dcortes <dcortes@student.42lausanne.ch>
2024-12-12 15:22:02 +0100

    feat(ConfigParserNode): add

--- inc/config/parser/ConfigParserNode.hpp
+++ inc/config/parser/ConfigParserNode.hpp
@@ -4,7 +4,10 @@
 class	ConfigParserNode
 {
 	public:
-		virtual	~ConfigParserNode(void) = 0;
+		virtual			~ConfigParserNode(void) = 0;
+
+		virtual void	display(void) const = 0;
+		virtual void	display(int nestingLevel) const = 0;
 };
 
 #endif
